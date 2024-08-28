a4a144790487f7a8797e58bbbc1d10f1b8fe3de4
lewypopescu <popesculewy@gmail.com>
2024-08-28 20:22:05 +0200

    done, server ok, commands ok

--- contacts.js
+++ contacts.js
@@ -1,16 +1,24 @@
-const { ifError } = require("assert");
-const { isUtf8 } = require("buffer");
-const { error } = require("console");
-const fs = require("fs");
-const path = require("path");
+import { nanoid } from "nanoid";
+
+import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// const fs = require("fs");
+// const path = require("path");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-function listContacts() {
+export function listContacts() {
   return new Promise((resolve, reject) => {
-    fs.readFile(contactsPath, "utf8", (error, data) => {
-      if (error) {
-        reject(error);
+    fs.readFile(contactsPath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
       } else {
         resolve(JSON.parse(data));
       }
@@ -18,27 +26,26 @@ function listContacts() {
   });
 }
 
-function getContactsById(contactId) {
+export function getContactById(contactId) {
   return new Promise((resolve, reject) => {
-    fs.readFile(contactsPath, "utf8", (error, data) => {
-      if (error) {
-        reject(error);
+    fs.readFile(contactsPath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
       }
-      return;
+      const contacts = JSON.parse(data);
+      const contact = contacts.find((contact) => contact.id === contactId);
+
+      if (contact) {
+        resolve(contact);
+      } else {
+        resolve(null);
+      }
     });
-
-    const contacts = JSON.parse(data);
-    const contact = contacts.find((contact) => contact.id === contactId);
-
-    if (contact) {
-      resolve(contact);
-    } else {
-      resolve(null);
-    }
   });
 }
 
-function removeContact(contactId) {
+export function removeContact(contactId) {
   return new Promise((resolve, reject) => {
     fs.readFile(contactsPath, "utf8", (err, data) => {
       if (err) {
@@ -51,16 +58,48 @@ function removeContact(contactId) {
 
       if (index !== -1) {
         const removedContact = contacts.splice(index, 1)[0];
-        fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(removedContact);
+        fs.writeFile(
+          contactsPath,
+          JSON.stringify(contacts, null, 2),
+          (error) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(removedContact);
+            }
           }
-        });
+        );
       } else {
         resolve(null);
       }
+    });
+  });
+}
+
+export function addContact(name, email, phone) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(contactsPath, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      const contacts = JSON.parse(data);
+      const newContact = {
+        id: nanoid(),
+        name,
+        email,
+        phone,
+      };
+      contacts.push(newContact);
+
+      fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(newContact);
+        }
+      });
     });
   });
 }
--- index.js
+++ index.js
@@ -1,4 +1,75 @@
-console.log("Hello, blue");
-console.log("Hey Node");
-const a = 22;
-console.log(a);
+import * as contacts from "./contacts.js";
+
+import { Command } from "commander";
+
+// contacts
+//   .listContacts()
+//   .then((data) => console.log("Contacts:", data))
+//   .catch((err) => console.error("Error:", err));
+
+// contacts
+//   .getContactById("C9sjBfCo4UJCWjzBnOtxl")
+//   .then((contact) => console.log("Contact By Id:", contact))
+//   .catch((err) => console.log("Error:", err));
+
+// contacts
+//   .removeContact("05olLMgyVQdWRwgKfg5J6")
+//   .then((removedContact) => console.log("Deleted Contact:", removedContact))
+//   .catch((err) => console.log("Error:", err));
+
+// contacts
+//   .addContact("Hey Hey", "hey@mail.com", "1234567891")
+//   .then((newContact) => console.log("New Contact:", newContact))
+//   .catch((err) => console.log("Error:", err));
+
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts
+        .listContacts()
+        .then((data) => console.log("Contacts:", data))
+        .catch((err) => console.error("Error:", err));
+      break;
+
+    case "get":
+      contacts
+        .getContactById(id)
+        .then((contact) => console.log("Contact By Id:", contact))
+        .catch((err) => console.log("Error:", err));
+      break;
+
+    case "add":
+      contacts
+        .addContact(name, email, phone)
+        .then((newContact) => console.log("New Contact:", newContact))
+        .catch((err) => console.log("Error:", err));
+
+      break;
+
+    case "remove":
+      contacts
+        .removeContact(id)
+        .then((removedContact) =>
+          console.log("Deleted Contact:", removedContact)
+        )
+        .catch((err) => console.log("Error:", err));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
